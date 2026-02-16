ac1d2a41bfb58c0851966ef6a7451f26b55dfbf4
JIPalliano <ismael_palliano190@hotmail.com>
2026-02-16 17:08:03 -0300

    começando o projeto

---
+++ carbigdata/src/main/java/com/carbigdata/CarbigdataApplication.java
@@ -0,0 +1,13 @@
+package com.carbigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbigdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbigdataApplication.class, args);
+    }
+
+}
---
+++ carbigdata/src/test/java/com/carbigdata/CarbigdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbigdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
